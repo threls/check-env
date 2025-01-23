e327132abb80e4fa515092bada1e218236cf12bd
sabina1997 <41695168+sabina1997@users.noreply.github.com>
2025-01-23 16:41:06 +0000

    Fix styling

--- src/Commands/ThrelsCheckEnvCommand.php
+++ src/Commands/ThrelsCheckEnvCommand.php
@@ -12,10 +12,9 @@ class ThrelsCheckEnvCommand extends Command
 
     public $description = 'My command';
 
-
     public function handle()
     {
-        $this->info("Starting environment file validation...");
+        $this->info('Starting environment file validation...');
 
         $suffix = config('check-env.temp-env-suffix');
 
@@ -29,6 +28,7 @@ class ThrelsCheckEnvCommand extends Command
         }
 
         $this->info("\nEnvironment file validation completed successfully for all environments.");
+
         return self::SUCCESS;
     }
 
@@ -37,8 +37,9 @@ class ThrelsCheckEnvCommand extends Command
         $encryptedFile = base_path(".env.$env.encrypted");
         $testEncryptedFile = base_path(".env.$env.$suffix.encrypted");
 
-        if (!File::exists($encryptedFile)) {
+        if (! File::exists($encryptedFile)) {
             Log::error("Encrypted file not found: $encryptedFile");
+
             return;
         }
 
@@ -56,6 +57,7 @@ class ThrelsCheckEnvCommand extends Command
 
         if ($resultCode !== 0) {
             Log::error("Failed to decrypt the .env.$env.$suffix.encrypted file.");
+
             return;
         }
 
@@ -68,8 +70,9 @@ class ThrelsCheckEnvCommand extends Command
         $testEnvFile = base_path(".env.$env.$suffix");
         $testEncryptedFile = base_path(".env.$env.$suffix.encrypted");
 
-        if (!File::exists($envFile) || !File::exists($testEnvFile)) {
+        if (! File::exists($envFile) || ! File::exists($testEnvFile)) {
             Log::error("One or both files are missing: $envFile, $testEnvFile");
+
             return;
         }
 
@@ -81,6 +84,7 @@ class ThrelsCheckEnvCommand extends Command
 
         if ($envContent !== $decryptedContent) {
             Log::error("The .env.$env and decrypted .env.$env.encrypted do not match.");
+
             return;
         }
 
