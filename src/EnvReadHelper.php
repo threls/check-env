b7c88d4f6af1825cc873ac40882afed6a5752332
sabina1997 <41695168+sabina1997@users.noreply.github.com>
2025-02-13 23:35:56 +0000

    Fix styling

--- src/EnvReadHelper.php
+++ src/EnvReadHelper.php
@@ -1,17 +1,17 @@
 <?php
 
-if (!function_exists('getEnvKeyFromFile')) {
+if (! function_exists('getEnvKeyFromFile')) {
     function getEnvKeyFromFile($fileName, $key): ?string
     {
         $filePath = base_path($fileName);
-        if (!file_exists($filePath)) {
+        if (! file_exists($filePath)) {
             return null;
         }
 
         $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($lines as $line) {
-            if (str_starts_with($line, $key . '=')) {
-                return trim(str_replace($key . '=', '', $line), '"');
+            if (str_starts_with($line, $key.'=')) {
+                return trim(str_replace($key.'=', '', $line), '"');
             }
         }
 
